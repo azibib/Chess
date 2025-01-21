4f89643c8849e2a42c6d98fd87d6707c32872059
“azibib <bibbinsazi@gmail.com>
2025-01-21 00:45:33 -0500

    king can move infront of pawn to avoid check

--- src/main/java/chess/Pieces/King.java
+++ src/main/java/chess/Pieces/King.java
@@ -31,7 +31,10 @@ public class King extends Piece{
             if(p.getAlliance()==this.pieceAlliance){continue;}
             if(p instanceof King){continue;}//this causes a infinite loop
             for(Move move : p.calculateMoves(board)){
-                cantMove.add(move.newTile());
+                if(!(p instanceof Pawn)){
+                    cantMove.add(move.newTile());
+                }
+                
                 if(p instanceof Pawn&&(move.newTile()!=p.getPosition()+8||move.newTile()!=p.getPosition()-8)){
                     
                     cantMove.add(move.newTile()-1);
